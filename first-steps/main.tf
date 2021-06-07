8e05715e7cfc30dcc5855533659ffa55b67f4729
Adrian Malaguti <adrianma78@hotmail.com>
2021-06-07 16:29:12 -0300

    set minimal tf required version

--- first-steps/main.tf
+++ first-steps/main.tf
@@ -7,6 +7,7 @@ terraform {
       version = "=2.61.0"
     }
   }
+  required_version = ">= 0.14"
 }
 
 # Configure the Microsoft Azure Provider
@@ -16,7 +17,6 @@ provider "azurerm" {
       delete_os_disk_on_deletion = true
     }
   }
-
   subscription_id = var.subscription_id
 }
 
