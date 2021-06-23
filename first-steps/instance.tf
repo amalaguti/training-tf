47d8b35b782f2d3bfb7445bf6236322860cf9f56
Adrian Malaguti <adrianma78@hotmail.com>
2021-06-23 10:42:51 -0300

    ubuntu 18.04 latest

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -10,7 +10,7 @@ resource "azurerm_linux_virtual_machine" "demo-instance" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
