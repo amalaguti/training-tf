5d2bfc5427abf89f21b68cd480ae7f1534990222
Adrian Malaguti <adrianma78@hotmail.com>
2021-06-23 10:44:37 -0300

    my-build initial deploy

---
+++ my-build/README.md
@@ -0,0 +1,10 @@
+# First steps
+Create the basic resources to deploy an instance
+resource group
+virtual network
+subnet
+security group
+linux vm
+network interface
+public ip
+subnet secutiry group assoc
---
+++ my-build/main.tf
@@ -0,0 +1,27 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.61.0"
+    }
+  }
+  required_version = ">= 0.14"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {
+    virtual_machine {
+      delete_os_disk_on_deletion = true
+    }
+  }
+  subscription_id = var.subscription_id
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "demo" {
+  name     = var.prefix
+  location = var.location
+}
